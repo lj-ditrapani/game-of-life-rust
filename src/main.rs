c19f31d56f497441ed9b45c0a6c8717a3def203c
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-10-26 17:35:02 -0400

    Add some tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 /*  Author:  Lyall Jonathan Di Trapani ----------------------------------------
  *
  */
+
+type Board = [[Cell; 10]; 10];
+
 #[derive(Clone,Copy,Debug)]
 struct Cell {
     live: bool,
@@ -8,24 +11,59 @@ struct Cell {
 }
 
 impl Cell {
-    fn set_state(&mut self, live: bool) {
-        self.live = live;
+    fn update_neighbor_count(&self, board: Board) {
     }
 
-    fn set_neighbor_count(&mut self, count: u8) {
+    fn new() -> Cell {
+        Cell { live: false, neighbor_count: 0 }
+    }
+
+    fn set(&mut self, live: bool, count: u8) {
+        self.live = live;
         self.neighbor_count = count;
     }
 }
 
 fn main() {
-    let mut board = [[Cell { live: false, neighbor_count: 0}; 10]; 10];
+    let mut board = [[Cell::new(); 10]; 10];
     println!("{:?}", board);
     println!("\n\n{:?}", board[0]);
     board[0][0] = Cell { live: true, neighbor_count: 255 };
 
     println!("\n\n{:?}", board[0][0]);
 
-    board[0][1].set_state(true);
-    board[0][1].set_neighbor_count(16);
+    board[0][1].live = true;
+    board[0][1].neighbor_count = 14;
     println!("\n\n{:?}", board[0][1]);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Cell;
+
+    #[test]
+    fn new_cell() {
+        let mut c = Cell::new();
+        assert_eq!(c.live, false);
+        assert_eq!(c.neighbor_count, 0);
+    }
+
+    #[test]
+    fn set_cell() {
+        let mut c = Cell::new();
+        c.set(true, 7);
+        assert_eq!(c.live, true);
+        assert_eq!(c.neighbor_count, 7);
+    }
+
+    #[test]
+    fn get_neighbor_count() {
+        let mut board = [[Cell::new(); 10]; 10];
+        let mut c = board[0][0];
+        c.update_neighbor_count(board);
+        assert_eq!(c.neighbor_count, 0);
+        board[0][1].set(true, 0);
+        c.update_neighbor_count(board);
+        assert_eq!(c.neighbor_count, 1);
+    }
 }
